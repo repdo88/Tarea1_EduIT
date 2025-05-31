e524f507ac97d55de291b3fd502c847b11d337c2
Repdo <pedro.blason@outlook.com>
2025-05-31 16:09:43 -0300

    funca

--- Assets/PelotaPrincipal.cs
+++ Assets/PelotaPrincipal.cs
@@ -6,11 +6,29 @@ public class PelotaPrincipal : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     private int cuenta = 0;
+
+    // aca limito el movimiento de la pelota
+    public Transform plano;
+
+    private float limIzquierda;
+    private float limDerecha;
+    private float ancho;
     // Start is called before the first frame update
     void Start()
     {
 
-        rb.GetComponent<Rigidbody>();        
+        rb.GetComponent<Rigidbody>();
+
+        //Aca limito el movimiento de la pelota
+        Renderer planoRenderer = plano.GetComponent<Renderer>();
+        float anchoPlano = planoRenderer.bounds.size.z;
+
+        Renderer chekRenderer = GetComponent<Renderer>();
+        ancho = chekRenderer.bounds.size.z / 2f;
+
+        float centro = plano.position.z;
+        limIzquierda = centro - (anchoPlano / 2f) + ancho;
+        limDerecha = centro + (anchoPlano / 2f) - ancho;
     }
 
     // Update is called once per frame
@@ -20,6 +38,11 @@ public class PelotaPrincipal : MonoBehaviour
         {
             rb.useGravity = true;
         }
+
+        //Aca limito el movimiento de la pelota
+        Vector3 pos = this.transform.position;
+        pos.z = Mathf.Clamp(pos.z, limIzquierda, limDerecha);
+        this.transform.position = pos;
     }
 
     private void OnTriggerEnter(Collider collider)
@@ -34,6 +57,7 @@ public class PelotaPrincipal : MonoBehaviour
         if (collider.gameObject.CompareTag("Finish"))
         {
             print("Total de puntos: " + cuenta);
+            Destroy(gameObject);
         }
 
 
